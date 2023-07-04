bbe32a01f295aab6ae4b02e008ce03bc26d46506
Malo <malolc2001@gmail.com>
2023-07-04 12:58:06 +0200

    ca marche les reufs

--- src/main/scala/Parser.scala
+++ src/main/scala/Parser.scala
@@ -4,6 +4,11 @@ import zio._
 import zio.nio.file._
 import java.nio.charset.StandardCharsets
 import java.nio.file.{Files, Paths}
+import sudoku.Main.SudokuGrid.{Grid, emptyGrid}
+import io.circe._
+import io.circe.generic.auto._
+import io.circe.parser._
+import io.circe.syntax._
 
 object Parser {
 
@@ -16,5 +21,20 @@ object Parser {
         else ZIO.succeed(Files.readAllBytes(Paths.get(path)))
       }
       .map(bytes => new String(bytes, StandardCharsets.UTF_8))
+
+  def readGridFromFile(file: String): Either[Exception, Grid] = {
+    decode[Grid](file)
+      .fold(
+        error => Left(new Exception(error)),
+        grid => Right(grid)
+      )
+  }
+
+  def printGrid(grid: Grid): Task[Unit] = {
+    val gridString = grid
+      .map(row => row.map(cell => cell.getOrElse(" ")).mkString(" "))
+      .mkString("\n")
+    ZIO.succeed(println(gridString))
+  }
 
 }
