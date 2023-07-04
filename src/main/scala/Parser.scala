bd3ca431e62a8e8651da3c79814b371f9c6f2a18
Enrique <73123851+F4l4X@users.noreply.github.com>
2023-07-04 14:30:05 +0200

    adding function to read json file

--- src/main/scala/Parser.scala
+++ src/main/scala/Parser.scala
@@ -4,8 +4,16 @@ import zio._
 import zio.nio.file._
 import java.nio.charset.StandardCharsets
 import java.nio.file.{Files, Paths}
+import zio.json._
+import zio.json.JsonDecoderOps._
 
 object Parser {
+
+  // Define a case class to represent a cell in the Sudoku grid
+  case class Cell(value: Option[Int])
+
+  // Define a decoder for the Cell case class
+  implicit val cellDecoder: JsonDecoder[Cell] = DeriveJsonDecoder.gen[Cell]
 
   def readFile(path: String): Task[String] =
     ZIO
@@ -16,5 +24,8 @@ object Parser {
         else ZIO.succeed(Files.readAllBytes(Paths.get(path)))
       }
       .map(bytes => new String(bytes, StandardCharsets.UTF_8))
-
+  def parseSudoku(json: String): Task[Vector[Vector[Cell]]] =
+    ZIO.fromEither(
+      json.fromJson[List[List[Option[Int]]]].map(_.map(_.map(Cell)))
+    )
 }
